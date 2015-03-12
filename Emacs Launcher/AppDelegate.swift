ff98467be66be36e66ca8da077876ee10d5d4e46
Billy He <qiushihe@me.com>
2015-03-12 13:48:55 -0700

    Initial commit

--- Emacs Launcher/AppDelegate.swift
+++ Emacs Launcher/AppDelegate.swift
@@ -6,22 +6,23 @@
 //  Copyright (c) 2015 Billy He. All rights reserved.
 //
 
+// https://nsrover.wordpress.com/2014/10/10/creating-a-os-x-menubar-only-app
+
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
+    @IBOutlet weak var window: NSWindow!;
+    
+    var statusItem: NSStatusItem!;
+    var darkModeOn: Bool!;
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength);
+        self.statusItem.image = NSImage(named: "switchIcon.png");
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
 }
-
