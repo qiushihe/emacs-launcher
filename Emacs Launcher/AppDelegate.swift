da2a96dc433d3f3729a1ce2085fef6124d70a0f8
Billy He <qiushihe@me.com>
2015-03-12 13:19:56 -0700

    Initial Commit

---
+++ Emacs Launcher/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Emacs Launcher
+//
+//  Created by Billy He on 3/12/15.
+//  Copyright (c) 2015 Billy He. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
